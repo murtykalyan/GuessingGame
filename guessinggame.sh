2a5b7880fc0c11f10ef1cdc33aecd0a236af2030
MurthyKalyan <murtykalyan@gmail.com>
2020-09-24 23:04:56 +0530

    Updated guessinggame.sh for consider only numerics

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 function game(){
-    ans=$(ls -l |grep "^-"|wc -l)
+    ans=$(ls -la |grep "^-"|wc -l)
     echo "Welcome to the GuessingGame"
     n=0
     add=1
@@ -10,15 +10,19 @@ function game(){
         echo "Guess the number of files?"
         read guess
         n=`expr $n + $add`
-        if [ $guess -gt $ans ]
-        then
-            echo "Your guess exceeds than the number of files!"
-        elif [ $guess -lt $ans ]
-        then
-            echo "Your guess value is less than the number of files!"
+        if echo $guess | egrep -q '^[0-9]+$'; then
+            if [ $guess -gt $ans ]
+            then
+                echo "Your guess exceeds than the number of files!"
+            elif [ $guess -lt $ans ]
+            then
+                echo "Your guess value is less than the number of files!"
+            else
+                echo "Hurrayy, Your guess is correct. You took $n times to guess correctly."
+            break;
+            fi
         else
-            echo "Hurrayy, Your guess is correct. You took $n times to guess correctly."
-        break;
+            echo "Please enter number, Wrong format detected"
         fi
     done
 }
